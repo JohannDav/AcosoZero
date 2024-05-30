5264e78d4f76cee20362ad2ba5392d41deef1d54
lolxdhola78 Gutierrez <gutierrez.flores.cristian.2006@gmail.com>
2024-05-30 02:47:10 -0600

    Mandar gmail

--- sfrentas/build/web/index.jsp
+++ sfrentas/build/web/index.jsp
@@ -6,7 +6,7 @@
       <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
       <link rel="icon" type="image/x-icon" href="AcosoZer0Batiz/Imagenes/favicon.ico">
       <link href="css/IndexDesigns.css" rel="stylesheet" type="text/css" />
-      <link href="css/OverallDesigns222.css" rel="stylesheet" type="text/css" />
+      <link href="css/OverallDesigns1.css" rel="stylesheet" type="text/css" />
       <link href="https://fonts.googleapis.com/icon?family=Material+Icons&quot; rel="stylesheet">
       <link href="css/Buscar_Reportes.css" rel="stylesheet" type="text/css" />
   <script src="https://code.jquery.com/jquery-2.2.4.min.js"></script>
@@ -226,16 +226,49 @@
                   <div class="text-center">
                      <img src="img/user.png" width="100" height="100" />
                   </div>
-                  <div class="modal-header text-center">
-                     <ul class="nav nav-pills">
-                        <li class="ButtonFormSession">
-                           <button id="InicioSesionForm">Iniciar Sesisón</button>
-                        </li>
-                        <li class="ButtonFormSession">
-                           <button id="RegistroForm">Registro</button>
-                        </li>
-                     </ul>
-                  </div>
+                   <div class="olapapu">
+                   <svg xmlns="http://www.w3.org/2000/svg" version="1.1">
+  <defs>
+    <filter id="goo">
+      <feGaussianBlur in="SourceGraphic" result="blur" stdDeviation="10"></feGaussianBlur>
+      <feColorMatrix in="blur" mode="matrix" values="1 0 0 0 0 0 1 0 0 0 0 0 1 0 0 0 0 0 21 -7" result="goo"></feColorMatrix>
+      <feBlend in2="goo" in="SourceGraphic" result="mix"></feBlend>
+    </filter>
+  </defs>
+</svg></div>
+<div class="modal-header text-center">
+<div class="buttons">
+<ul class="nav nav-pills">
+
+                           <li class="ButtonFormSession">
+
+  <button class="blob-btn" id="InicioSesionForm">
+Inicio    <span class="blob-btn__inner">
+      <span class="blob-btn__blobs">
+        <span class="blob-btn__blob"></span>
+        <span class="blob-btn__blob"></span>
+        <span class="blob-btn__blob"></span>
+        <span class="blob-btn__blob"></span>
+      </span>
+    </span></button></li>
+  
+    <li class="ButtonFormSession">
+  <button class="blob-btn" id="RegistroForm">
+Registro
+<span class="blob-btn__inner">
+      <span class="blob-btn__blobs">
+        <span class="blob-btn__blob"></span>
+        <span class="blob-btn__blob"></span>
+        <span class="blob-btn__blob"></span>
+        <span class="blob-btn__blob"></span>
+      </span>
+    </span>
+  </button></li></ul>
+</div>
+
+
+</div>
+
                   <div class="modal-body">
                      <div class="tab-content" id="pills-tabContent">
                         <!-- Iniciar Session -->
--- sfrentas/src/java/PDFs/GeneratePdfServlet.java
+++ sfrentas/src/java/PDFs/GeneratePdfServlet.java
@@ -1,4 +1,5 @@
 package PDFs;
+
 import java.util.Date;
 import java.text.SimpleDateFormat;
 import java.io.IOException;
@@ -13,97 +14,83 @@ import com.itextpdf.text.Paragraph;
 import com.itextpdf.text.pdf.PdfPCell;
 import com.itextpdf.text.pdf.PdfPTable;
 import com.itextpdf.text.pdf.PdfWriter;
+import java.io.ByteArrayOutputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.Properties;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Multipart;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeBodyPart;
+import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMultipart;
+import javax.activation.DataHandler;
+import javax.activation.DataSource;
+import javax.mail.util.ByteArrayDataSource;
 
 @WebServlet("/ReporteServletActionInsertReport")
 public class GeneratePdfServlet extends HttpServlet {
     private static final String jdbcURL = "jdbc:mysql://localhost:3306/bdsfrentas";
     private static final String jdbcUsername = "root";
     private static final String jdbcPassword = "n0m3l0";
+    
+    private static final String fromEmail = "acosozer0batiz@gmail.com";
+    private static final String emailPassword = "zhmakgmcvqtsjqoj";
 
-    /**
-     *
-     * @param request
-     * @param response
-     * @throws ServletException
-     * @throws IOException
-     */
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        // Establecer el tipo de contenido como PDF
         response.setContentType("application/pdf");
 
-        // Establecer el encabezado Content-Disposition para descargar el PDF
-
-        // Obtener el correo electrónico del usuario desde la solicitud
         String correo = request.getParameter("correo");
-        // Obtener el ID del reporte desde la solicitud
         String reportId = request.getParameter("idReporte");
 
-        // Variables para almacenar los datos del reporte
         String nombresReporte = "";
         String descripcion = "";
         String precio = "";
         String cantreporte = "";
 
-        // Variables para almacenar los datos del usuario
         String boleta = "";
         String nombresUsuario = "";
         String direccion = "";
         String email = "";
 
-        // Establecer la conexión a la base de datos
         try (Connection connection = DriverManager.getConnection(jdbcURL, jdbcUsername, jdbcPassword)) {
-            // Crear la consulta SQL para obtener los datos del reporte
             String sqlReporte = "SELECT Nombres, Descripcion, Precio, Cantreporte FROM reporte WHERE idReporte = ?";
             
-            // Preparar la consulta para el reporte
             try (PreparedStatement statementReporte = connection.prepareStatement(sqlReporte)) {
                 statementReporte.setString(1, reportId);
 
-                // Ejecutar la consulta del reporte y obtener el resultado
                 try (ResultSet resultSetReporte = statementReporte.executeQuery()) {
-                    // Verificar si se encontró un resultado para el reporte
                     if (resultSetReporte.next()) {
-                        // Asignar los valores obtenidos a las variables del reporte
                         nombresReporte = resultSetReporte.getString("Nombres");
                         descripcion = resultSetReporte.getString("Descripcion");
                         precio = resultSetReporte.getString("Precio");
                         cantreporte = resultSetReporte.getString("Cantreporte");
-                        
-                        
                     }
                 }
             }
 
-            // Crear la consulta SQL para obtener los datos del usuario por correo electrónico
             String sqlUsuario = "SELECT Boleta, Nombres, Direccion, Email, Password FROM usuario WHERE Email = ?";
 
-            // Preparar la consulta para el usuario
             try (PreparedStatement statementUsuario = connection.prepareStatement(sqlUsuario)) {
                 statementUsuario.setString(1, correo);
 
-                // Ejecutar la consulta del usuario y obtener el resultado
                 try (ResultSet resultSetUsuario = statementUsuario.executeQuery()) {
-                    // Verificar si se encontró un resultado para el usuario
                     if (resultSetUsuario.next()) {
-                        // Asignar los valores obtenidos a las variables del usuario
                         boleta = resultSetUsuario.getString("Boleta");
                         nombresUsuario = resultSetUsuario.getString("Nombres");
                         direccion = resultSetUsuario.getString("Direccion");
                         email = resultSetUsuario.getString("Email");
 
-        // Obtener el nombre del usuario desde la solicitud
-
-        // Configurar el nombre del archivo PDF con el nombre del usuario y su BOLETA
-        String fileName = "Reporte_Acoso_" + nombresUsuario + "_" + boleta + ".pdf";
-
-        // Establecer el encabezado Content-Disposition para descargar el PDF con el nombre del archivo
-        response.setHeader("Content-Disposition", "attachment; filename=" + fileName);
+                        String fileName = "Reporte_Acoso_" + nombresUsuario + "_" + boleta + ".pdf";
+                        response.setHeader("Content-Disposition", "attachment; filename=" + fileName);
                     }
                 }
             }
@@ -111,73 +98,62 @@ public class GeneratePdfServlet extends HttpServlet {
             throw new ServletException("Error en la conexión a la base de datos", e);
         }
 
-        // Generar el PDF con los datos obtenidos del reporte y del usuario
+        byte[] pdfData = null;
+
         try {
             Document document = new Document();
-            PdfWriter.getInstance(document, response.getOutputStream());
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            PdfWriter.getInstance(document, baos);
             document.open();
-            
+
             Date fechaActual = new Date();
             SimpleDateFormat formatoFecha = new SimpleDateFormat("dd/MM/yyyy");
             String fechaFormateada = formatoFecha.format(fechaActual);
 
-
-            // Crear la tabla para los datos del usuario
             PdfPTable tableUsuario = new PdfPTable(4);
             tableUsuario.setWidthPercentage(100);
 
-            // Primera fila
             PdfPCell cellDatosUsuario = new PdfPCell(new Paragraph("Datos del Usuario"));
             cellDatosUsuario.setColspan(2);
-            cellDatosUsuario.setHorizontalAlignment(Element.ALIGN_CENTER); // Centrar horizontalmente
+            cellDatosUsuario.setHorizontalAlignment(Element.ALIGN_CENTER);
             PdfPCell cellFecha = new PdfPCell(new Paragraph("Fecha"));
             PdfPCell cellFechaValor = new PdfPCell(new Paragraph(fechaFormateada));
 
-            // Agregar celdas a la primera fila
             tableUsuario.addCell(cellDatosUsuario);
             tableUsuario.addCell(cellFecha);
             tableUsuario.addCell(cellFechaValor);
 
-            // Segunda fila
             tableUsuario.addCell(new PdfPCell(new Paragraph("BOLETA:")));
             tableUsuario.addCell(new PdfPCell(new Paragraph(boleta)));
             tableUsuario.addCell(new PdfPCell(new Paragraph("Nombre:")));
             tableUsuario.addCell(new PdfPCell(new Paragraph(nombresUsuario)));
 
-            // Tercera fila
             tableUsuario.addCell(new PdfPCell(new Paragraph("Dirección:")));
             tableUsuario.addCell(new PdfPCell(new Paragraph(direccion)));
             tableUsuario.addCell(new PdfPCell(new Paragraph("Email:")));
             tableUsuario.addCell(new PdfPCell(new Paragraph(email)));
 
-            // Agregar la tabla al documento
             document.add(tableUsuario);
-            
-            // Agregar los datos del reporte al PDF
+
             Paragraph paragraphReporte = new Paragraph();
-                      PdfPTable tableReporte = new PdfPTable(4);
+            PdfPTable tableReporte = new PdfPTable(4);
             tableReporte.setWidthPercentage(100);
 
-            // Primera fila
             PdfPCell cellDatosReporte = new PdfPCell(new Paragraph("Datos del Reporte"));
             cellDatosReporte.setColspan(4);
-            cellDatosReporte.setHorizontalAlignment(Element.ALIGN_CENTER); // Centrar horizontalmente
+            cellDatosReporte.setHorizontalAlignment(Element.ALIGN_CENTER);
             PdfPCell cellNombreReporte = new PdfPCell(new Paragraph("Nombre del Reporte"));
             PdfPCell cellNombreReporteValor = new PdfPCell(new Paragraph(nombresReporte));
 
-            // Segunda fila
             PdfPCell cellDescripcion = new PdfPCell(new Paragraph("Descripción"));
             PdfPCell cellDescripcionValor = new PdfPCell(new Paragraph(descripcion));
 
-            // Tercera fila
             PdfPCell cellPrecio = new PdfPCell(new Paragraph("Precio"));
             PdfPCell cellPrecioValor = new PdfPCell(new Paragraph(String.valueOf(precio)));
 
-            // Cuarta fila
             PdfPCell cellCantreporte = new PdfPCell(new Paragraph("Cantreporte"));
             PdfPCell cellCantreporteValor = new PdfPCell(new Paragraph(String.valueOf(cantreporte)));
 
-            // Agregar celdas a la tabla
             tableReporte.addCell(cellDatosReporte);
             tableReporte.addCell(cellNombreReporte);
             tableReporte.addCell(cellNombreReporteValor);
@@ -188,16 +164,58 @@ public class GeneratePdfServlet extends HttpServlet {
             tableReporte.addCell(cellCantreporte);
             tableReporte.addCell(cellCantreporteValor);
 
-            // Agregar la tabla al documento
             document.add(tableReporte);
-
             document.add(paragraphReporte);
-// Obtener la fecha actual
-
 
             document.close();
+
+            pdfData = baos.toByteArray();
         } catch (Exception e) {
             throw new ServletException("Error al generar el PDF", e);
+        }
+
+        sendEmailWithAttachment("morales.munoz.mily@gmail.com", "Reporte Generado", "Adjunto se encuentra el reporte generado.", pdfData);
+
+        response.getOutputStream().write(pdfData);
+    }
+
+    private void sendEmailWithAttachment(String toEmail, String subject, String messageBody, byte[] attachmentData) throws ServletException {
+        Properties props = new Properties();
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.smtp.host", "smtp.gmail.com");
+        props.put("mail.smtp.port", "587");
+        props.put("mail.smtp.ssl.trust", "smtp.gmail.com");
+
+        Session session = Session.getInstance(props, new javax.mail.Authenticator() {
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication(fromEmail, emailPassword);
+            }
+        });
+
+        try {
+            Message message = new MimeMessage(session);
+            message.setFrom(new InternetAddress(fromEmail));
+            message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(toEmail));
+            message.setSubject(subject);
+
+            MimeBodyPart messageBodyPart = new MimeBodyPart();
+            messageBodyPart.setText(messageBody);
+
+            MimeBodyPart attachmentPart = new MimeBodyPart();
+            DataSource source = new ByteArrayDataSource(attachmentData, "application/pdf");
+            attachmentPart.setDataHandler(new DataHandler(source));
+            attachmentPart.setFileName("Reporte.pdf");
+
+            Multipart multipart = new MimeMultipart();
+            multipart.addBodyPart(messageBodyPart);
+            multipart.addBodyPart(attachmentPart);
+
+            message.setContent(multipart);
+
+            Transport.send(message);
+        } catch (MessagingException e) {
+            throw new ServletException("Error al enviar el correo electrónico", e);
         }
     }
 }
